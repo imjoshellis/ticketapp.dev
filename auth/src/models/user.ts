3ead484e927d7d681dbf095006ec25dfa246ad11
Josh Ellis <josh@imjoshellis.com>
2020-10-21 15:32:05 -0500

    Add toJSON

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -15,10 +15,22 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(newUser: NewUser): UserDoc
 }
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true }
-})
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+  },
+  {
+    toJSON: {
+      transform (_doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.password
+      },
+      versionKey: false
+    }
+  }
+)
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
