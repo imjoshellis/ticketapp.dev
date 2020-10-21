941203a62f1f6a764f5a897f37b72dea3904feac
Josh Ellis <josh@imjoshellis.com>
2020-10-21 14:04:48 -0500

    Add password hashing and custom interfaces

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,8 +1,33 @@
 import mongoose from 'mongoose'
+import { Password } from '../services'
+
+interface NewUser {
+  email: string
+  password: string
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(newUser: NewUser): UserDoc
+}
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true }
 })
 
-export const User = mongoose.model('User', userSchema)
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
+})
+
+userSchema.statics.build = (newUser: NewUser) => new User(newUser)
+
+export const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
