994e9ddd02f350e05203811ad898b99470933ca4
Josh Ellis <josh@imjoshellis.com>
2020-10-21 08:55:56 -0500

    Add basic validation to signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,26 @@
-import { Application } from 'express'
+import { Application, Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 export const addSignupRoute = (app: Application) => {
-  app.post('/api/users/signup', (_, res) => {
-    res.send('signup')
-  })
+  app.post(
+    '/api/users/signup',
+    [
+      body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+      body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 and 20 characters')
+    ],
+    (req: Request, res: Response) => {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array())
+      }
+      const { email, password } = req.body
+      console.log('Creating a user...')
+      return res.send({ email, password })
+    }
+  )
 }
