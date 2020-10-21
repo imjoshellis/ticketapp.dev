98a7ae55d109069e72de7159dc962ce51401def1
Josh Ellis <josh@imjoshellis.com>
2020-10-21 14:05:10 -0500

    Add real user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
-import { RequestValidationError } from '../errors'
 import { Application, NextFunction, Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
-import { DatabaseConnectionError } from '../errors'
+import { BadRequestError, RequestValidationError } from '../errors'
+import { User } from '../models'
 
 export const addSignUpRoute = (app: Application) => {
   app.post(
@@ -15,15 +15,21 @@ export const addSignUpRoute = (app: Application) => {
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
     ],
-    (req: Request, _res: Response, _next: NextFunction) => {
+    async (req: Request, res: Response, _next: NextFunction) => {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array())
       }
-      // const { email, password } = req.body
-      console.log('Creating a user...')
-      throw new DatabaseConnectionError()
-      // return res.send({ email, password })
+
+      const { email, password } = req.body
+
+      const existingUser = await User.findOne({ email })
+      if (existingUser) throw new BadRequestError('Email already in use')
+
+      const user = User.build({ email, password })
+      await user.save()
+
+      return res.status(201).send(user)
     }
   )
 }
