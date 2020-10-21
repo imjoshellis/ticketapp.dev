475c0864d24f43b16445f25bc075497ba42b0ebc
Josh Ellis <josh@imjoshellis.com>
2020-10-21 15:32:32 -0500

    Update signin.ts

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,43 @@
-import { Application } from 'express'
+import { Application, Request, Response } from 'express'
+import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import { BadRequestError } from '../errors'
+import { User } from '../models'
+import { validateRequest } from './../middlewares'
 
 export const addSignInRoute = (app: Application) => {
-  app.post('/api/users/signin', (_, res) => {
-    res.send('signin')
-  })
+  app.post(
+    '/api/users/signin',
+    [
+      body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+      body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must supply a password')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+      const { email, password } = req.body
+
+      const existingUser = await User.findOne({ email })
+      if (existingUser) throw new BadRequestError('Email already in use')
+
+      const user = User.build({ email, password })
+      await user.save()
+
+      const userJwt = jwt.sign(
+        {
+          id: user.id,
+          email: user.email
+        },
+        process.env.JWT_KEY!
+      )
+
+      req.session = { userJwt }
+
+      return res.status(201).send(user)
+    }
+  )
 }
