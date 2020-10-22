f3fc9278f6a579ed911837339f0ed26aa9b19602
Josh Ellis <josh@imjoshellis.com>
2020-10-22 14:27:52 -0500

    Move to common package

--- auth/src/errors/index.ts
+++
@@ -1,5 +0,0 @@
-export { BadRequestError } from './BadRequestError'
-export { RequestValidationError } from './RequestValidationError'
-export { DatabaseConnectionError } from './DatabaseConnectionError'
-export { CustomError } from './CustomError'
-export { NotAuthorizedError } from './NotAuthorizedError'
--- auth/src/middlewares/index.ts
+++
@@ -1,4 +0,0 @@
-export { currentUser } from './currentUser'
-export { errorHandler } from './errorHandler'
-export { requireAuth } from './requireAuth'
-export { validateRequest } from './validateRequests'
