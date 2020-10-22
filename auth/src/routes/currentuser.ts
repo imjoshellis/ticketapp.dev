7c5269de8722b00969fc26f09f60c727a429d9f0
Josh Ellis <josh@imjoshellis.com>
2020-10-21 20:36:25 -0500

    Update currentuser.ts

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,14 +1,8 @@
-import jwt from 'jsonwebtoken'
+import { currentUser } from '../middlewares'
 import { Application } from 'express'
 
 export const addCurrentUserRoute = (app: Application) => {
-  app.get('/api/users/currentuser', (req, res) => {
-    if (!req.session?.userJwt) return res.send({ currentUser: null })
-    try {
-      const currentUser = jwt.verify(req.session.userJwt, process.env.JWT_KEY!)
-      return res.send({ currentUser })
-    } catch (e) {
-      return res.send({ currentUser: null })
-    }
+  app.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.status(200).send({ currentUser: req.currentUser || null })
   })
 }
