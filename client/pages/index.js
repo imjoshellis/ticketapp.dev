642a924aaa3c5c0e26a5582c5590f57b9f8fd887
Josh Ellis <josh@imjoshellis.com>
2020-10-22 12:57:17 -0500

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,19 @@
-import { NextPage } from 'next'
 import React from 'react'
+import buildClient from '../api/buildClient'
 
-export const Index: NextPage = () => {
-  return <div>welcome home</div>
+export const Index = ({ currentUser }) => {
+  return (
+    <div>
+      <h1>welcome home</h1>
+      {currentUser ? `signed in as ${currentUser.email}` : 'not signed in'}
+    </div>
+  )
+}
+
+Index.getInitialProps = async context => {
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser')
+  return data
 }
 
 export default Index
