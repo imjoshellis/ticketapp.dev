6d60471072b6060bd1510f2b0ce879e5d3c64073
Josh Ellis <josh@imjoshellis.com>
2020-10-22 13:29:25 -0500

    Add header to _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/buildClient'
+import Header from '../components/header'
 
-export const App = ({ Component, pageProps }) => (
-  <div className='container'>
-    <Component {...pageProps} />
-  </div>
+export const App = ({ Component, pageProps, currentUser }) => (
+  <>
+    <Header currentUser={currentUser} />
+    <section className='container'>
+      <Component {...pageProps} />
+    </section>
+  </>
 )
+
+App.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx)
+  const {
+    data: { currentUser }
+  } = await client.get('/api/users/currentuser')
+  let pageProps = {}
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
+
+  return { pageProps, currentUser }
+}
 
 export default App
