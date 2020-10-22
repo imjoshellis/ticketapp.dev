5221fcc5a9e1d321ac467a8ef1ed7ab84c4d7fe3
Josh Ellis <josh@imjoshellis.com>
2020-10-22 13:29:17 -0500

    Create header.js

---
+++ client/components/header.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'sign up', href: '/auth/signup' },
+    !currentUser && { label: 'sign in', href: '/auth/signin' },
+    currentUser && { label: 'sign out', href: '/auth/signout' }
+  ]
+    .filter(link => link)
+    .map(({ label, href }) => (
+      <li>
+        <Link href={href}>
+          <a className='nav-link'>{label}</a>
+        </Link>
+      </li>
+    ))
+  return (
+    <nav className='navbar navbar-light bg-light container'>
+      <Link href='/'>
+        <a className='navbar-brand'>Home</a>
+      </Link>
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
+      </div>
+    </nav>
+  )
+}
