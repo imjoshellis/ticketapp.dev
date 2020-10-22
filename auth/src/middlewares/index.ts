d057d2817a528354952df6a435e6058353b5f465
Josh Ellis <josh@imjoshellis.com>
2020-10-21 20:35:15 -0500

    Update index.ts

--- auth/src/middlewares/index.ts
+++ auth/src/middlewares/index.ts
@@ -1,3 +1,4 @@
-export { validateRequest } from './validateRequests'
-export { errorHandler } from './errorHandler'
 export { currentUser } from './currentUser'
+export { errorHandler } from './errorHandler'
+export { requireAuth } from './requireAuth'
+export { validateRequest } from './validateRequests'
