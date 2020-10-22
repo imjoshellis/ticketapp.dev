36db6c3730ad4bbba2fc9bc615ff67c64e5c7baf
Josh Ellis <josh@imjoshellis.com>
2020-10-21 20:36:55 -0500

    Create signOut.test.ts

---
+++ auth/src/routes/__test__/signOut.test.ts
@@ -0,0 +1,16 @@
+import { testUserSignUp } from './../../test/setup'
+import request from 'supertest'
+import { app } from '../../app'
+
+it('clears cookie after signing out', async () => {
+  await testUserSignUp()
+
+  const res = await request(app)
+    .post('/api/users/signout')
+    .send({})
+    .expect(200)
+
+  expect(res.get('Set-Cookie')[0]).toEqual(
+    'express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
+  )
+})
