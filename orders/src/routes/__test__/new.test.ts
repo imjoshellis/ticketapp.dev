3667415e7d390020f8567afaf24800d3860a0b26
Josh Ellis <josh@imjoshellis.com>
2020-10-23 14:44:26 -0500

    Add new and testing

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,45 @@
+import { generateUserCookie } from './../../test/setup'
+import req from 'supertest'
+import { app } from '../../app'
+import mongoose from 'mongoose'
+import { Order, OrderStatus, Ticket } from '../../models'
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = mongoose.Types.ObjectId()
+  await req(app)
+    .post('/api/orders')
+    .set('Cookie', generateUserCookie())
+    .send({ ticketId })
+    .expect(404)
+})
+
+it('returns an error if the ticket is reserved', async () => {
+  const ticket = Ticket.build({ title: 'title', price: 20 })
+  await ticket.save()
+  const order = Order.build({
+    userId: 'aawftahwfiet',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+    ticket
+  })
+  await order.save()
+
+  await req(app)
+    .post('/api/orders')
+    .set('Cookie', generateUserCookie())
+    .send({ ticketId: ticket.id })
+    .expect(400)
+})
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({ title: 'title', price: 20 })
+  await ticket.save()
+
+  await req(app)
+    .post('/api/orders')
+    .set('Cookie', generateUserCookie())
+    .send({ ticketId: ticket.id })
+    .expect(201)
+})
+
+it.todo('emits an order created event')
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { body } from 'express-validator'
-import { requireAuth, validateRequest } from '@ije-ticketapp/common'
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest
+} from '@ije-ticketapp/common'
 import { Application, Request, Response } from 'express'
+import { body } from 'express-validator'
+import { Order, Ticket, OrderStatus } from '../models'
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
 
 export const addNewRoute = (app: Application) => {
   app.post(
@@ -13,8 +21,27 @@ export const addNewRoute = (app: Application) => {
         .withMessage('Ticket id must be defined')
     ],
     validateRequest,
-    async (_req: Request, res: Response) => {
-      res.send({})
+    async (req: Request, res: Response) => {
+      const { ticketId } = req.body
+
+      const ticket = await Ticket.findById(ticketId)
+      if (!ticket) throw new NotFoundError()
+      if (await ticket.isReserved())
+        throw new BadRequestError('Ticket is already reserved')
+
+      const expiresAt = new Date()
+      expiresAt.setSeconds(expiresAt.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
+      const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt,
+        ticket
+      })
+
+      await order.save()
+
+      res.status(201).send(order)
     }
   )
 }
