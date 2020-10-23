10e36bafc0096681c5577264108f2a23bde87cb5
Josh Ellis <josh@imjoshellis.com>
2020-10-23 14:44:15 -0500

    Create ticket.ts

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose'
+import { Order, OrderStatus } from '.'
+
+interface NewTicket {
+  title: string
+  price: number
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string
+  price: number
+  isReserved(): Promise<Boolean>
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(newTicket: NewTicket): TicketDoc
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    }
+  },
+  {
+    toJSON: {
+      transform (_, ret) {
+        ret.id = ret._id
+        delete ret._id
+      },
+      versionKey: false
+    }
+  }
+)
+
+ticketSchema.statics.build = (newTicket: NewTicket) => new Ticket(newTicket)
+
+ticketSchema.methods.isReserved = async function () {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete
+      ]
+    }
+  })
+  return !!existingOrder
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
+
+export { Ticket }
