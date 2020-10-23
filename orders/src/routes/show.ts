d7f69503a65a29815626d7cda04bc1e91b43fcdb
Josh Ellis <josh@imjoshellis.com>
2020-10-23 14:59:46 -0500

    Setup show route & tests

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,7 +1,20 @@
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError
+} from '@ije-ticketapp/common'
 import { Application, Request, Response } from 'express'
+import { Order } from '../models'
 
 export const addShowRoute = (app: Application) => {
-  app.get('/api/orders/:id', async (_req: Request, res: Response) => {
-    res.send({})
-  })
+  app.get(
+    '/api/orders/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+      const order = await Order.findById(req.params.id).populate('ticket')
+      if (!order) throw new NotFoundError()
+      if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError()
+      res.send(order)
+    }
+  )
 }
