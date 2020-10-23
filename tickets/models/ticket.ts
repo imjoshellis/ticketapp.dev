f8b0217338c093d64becbf5e566de75e2d72f1d1
Josh Ellis <josh@imjoshellis.com>
2020-10-22 22:05:46 -0500

    Create ticket.ts

---
+++ tickets/models/ticket.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+interface NewTicket {
+  title: string
+  price: number
+  userId: string
+}
+
+interface TicketDoc extends mongoose.Document {
+  title: string
+  price: number
+  userId: string
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(newTicket: NewTicket): TicketDoc
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    userId: { type: String, required: true }
+  },
+  {
+    toJSON: {
+      transform (_doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+      }
+    },
+    versionKey: false
+  }
+)
+
+ticketSchema.statics.build = (newTicket: NewTicket) => new Ticket(newTicket)
+
+export const Ticket = mongoose.model<TicketDoc, TicketModel>(
+  'Ticket',
+  ticketSchema
+)
