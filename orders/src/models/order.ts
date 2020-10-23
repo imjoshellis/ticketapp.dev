1558ff4856eedee9e9b3ef3bb5c568cbb3f9424c
Josh Ellis <josh@imjoshellis.com>
2020-10-23 14:44:11 -0500

    Move models to src

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose'
+import { OrderStatus } from '@ije-ticketapp/common'
+import { TicketDoc } from './ticket'
 
 interface NewOrder {
   userId: string
-  status: string
+  status: OrderStatus
   expiresAt: Date
   ticket: TicketDoc
 }
 
 interface OrderDoc extends mongoose.Document {
   userId: string
-  status: string
+  status: OrderStatus
   expiresAt: Date
   ticket: TicketDoc
 }
@@ -18,10 +20,15 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
   build(newOrder: NewOrder): OrderDoc
 }
 
-const ticketSchema = new mongoose.Schema(
+const orderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    status: { type: String, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created
+    },
     expiresAt: { type: mongoose.Schema.Types.Date },
     ticket: { type: mongoose.Schema.Types.ObjectId, ref: 'Ticket' }
   },
@@ -36,8 +43,8 @@ const ticketSchema = new mongoose.Schema(
   }
 )
 
-ticketSchema.statics.build = (newOrder: NewOrder) => new Order(newOrder)
+orderSchema.statics.build = (newOrder: NewOrder) => new Order(newOrder)
 
-const Order = mongoose.model<OrderDoc, OrderModel>('Order', ticketSchema)
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema)
 
-export { Order }
+export { Order, OrderStatus }
