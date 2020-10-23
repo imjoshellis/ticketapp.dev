7378655c7fc6195628d882c7d99177927ab49107
Josh Ellis <josh@imjoshellis.com>
2020-10-23 15:06:42 -0500

    Add delete route and test

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,61 @@
+import req from 'supertest'
+import { app } from '../../app'
+import { Order, OrderStatus, Ticket } from '../../models'
+import { generateUserCookie } from './../../test/setup'
+import mongoose from 'mongoose'
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({ price: 20, title: 'title' })
+  await ticket.save()
+  return ticket
+}
+
+it('returns 404 if order does not exist', async () => {
+  await req(app)
+    .delete('/api/orders/' + mongoose.Types.ObjectId())
+    .set('Cookie', generateUserCookie())
+    .expect(404)
+})
+
+it('returns 401 if order does not belong to user', async () => {
+  const ticket = await buildTicket()
+
+  const user1 = generateUserCookie()
+  const user2 = generateUserCookie()
+
+  const { body: order } = await req(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  await req(app)
+    .delete('/api/orders/' + order.id)
+    .set('Cookie', user2)
+    .expect(401)
+})
+
+it('cancels correct order for authorized user', async () => {
+  const ticket = await buildTicket()
+
+  const user1 = generateUserCookie()
+
+  const { body: createOrder } = await req(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  const { body: showOrder } = await req(app)
+    .delete('/api/orders/' + createOrder.id)
+    .set('Cookie', user1)
+    .expect(200)
+
+  expect(showOrder.id).toBe(createOrder.id)
+
+  const databaseOrder = await Order.findById(createOrder.id)
+
+  expect(databaseOrder?.status).toBe(OrderStatus.Cancelled)
+})
+
+it.todo('emits an OrderCanceled event')
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,7 +1,26 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth
+} from '@ije-ticketapp/common'
 import { Application, Request, Response } from 'express'
+import { Order } from '../models'
 
 export const addDeleteRoute = (app: Application) => {
-  app.delete('/api/orders/:id', async (_req: Request, res: Response) => {
-    res.send({})
-  })
+  app.delete(
+    '/api/orders/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+      const order = await Order.findById(req.params.id).populate('ticket')
+
+      if (!order) throw new NotFoundError()
+      if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError()
+
+      order.status = OrderStatus.Cancelled
+      await order.save()
+
+      res.send(order)
+    }
+  )
 }
