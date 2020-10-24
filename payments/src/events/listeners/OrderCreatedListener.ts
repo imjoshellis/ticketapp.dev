9e5fd05a47888a98395b5e7d60e111bd8baa72ad
Josh Ellis <josh@imjoshellis.com>
2020-10-24 14:22:28 -0500

    Create order:created listener

---
+++ payments/src/events/listeners/OrderCreatedListener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCreatedEvent, Subjects } from '@ije-ticketapp/common'
+import { QUEUE_GROUP_NAME } from './constants'
+import { Order } from '../../models'
+
+class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGroupName = QUEUE_GROUP_NAME
+  async onMessage (data: OrderCreatedEvent['data'], msg: Message) {
+    const {
+      id,
+      status,
+      userId,
+      version,
+      ticket: { price }
+    } = data
+    const order = Order.build({ id, status, price, userId, version })
+    await order.save()
+    msg.ack()
+  }
+}
+
+export { OrderCreatedListener }
---
+++ payments/src/events/listeners/__test__/OrderCreatedListener.test.ts
@@ -0,0 +1,44 @@
+import { OrderCreatedEvent, OrderStatus } from '@ije-ticketapp/common'
+import { natsWrapper } from '../../../natsWrapper'
+import { OrderCreatedListener } from '../OrderCreatedListener'
+import mongoose from 'mongoose'
+import { Message } from 'node-nats-streaming'
+import { Order } from '../../../models'
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client)
+
+  const data: OrderCreatedEvent['data'] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    userId: 'arst',
+    expiresAt: 'arst',
+    status: OrderStatus.Created,
+    ticket: {
+      id: 'arst',
+      price: 20
+    }
+  }
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  }
+
+  return { listener, data, msg }
+}
+
+it('correctly replicates the data', async () => {
+  const { listener, data, msg } = await setup()
+
+  listener.onMessage(data, msg)
+
+  const order = await Order.findById(data.id)
+
+  expect(order).toBeDefined()
+  if (!order) throw new Error('Order not found')
+
+  expect(order.status).toEqual(data.status)
+  expect(order.price).toEqual(data.ticket.price)
+  expect(order.userId).toEqual(data.userId)
+})
