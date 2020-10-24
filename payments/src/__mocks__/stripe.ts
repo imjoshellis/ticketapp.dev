c22eb397f04e2e74929d039e0d9516cffdacd2b3
Josh Ellis <josh@imjoshellis.com>
2020-10-24 16:23:02 -0500

    Add mock response

--- payments/src/__mocks__/stripe.ts
+++ payments/src/__mocks__/stripe.ts
@@ -1,5 +1,11 @@
 export const stripe = {
   charges: {
-    create: jest.fn().mockResolvedValue({})
+    create: jest
+      .fn()
+      .mockImplementation(opts =>
+        opts.source === 'tok_visa'
+          ? Promise.resolve()
+          : Promise.reject({ statusCode: 400, message: 'invalid token' })
+      )
   }
 }
