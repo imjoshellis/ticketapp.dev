bd3231cd526fe1b4ec1f3449d4067b8221361b26
Josh Ellis <josh@imjoshellis.com>
2020-10-24 15:50:30 -0500

    Add stripe api

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 import { Application, Request, Response } from 'express'
 import { body } from 'express-validator'
 import { Order } from '../models'
+import { stripe } from '../stripe'
 
 export const addNewRoute = (app: Application) => {
   app.post(
@@ -27,13 +28,19 @@ export const addNewRoute = (app: Application) => {
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-      const { orderId } = req.body
+      const { token, orderId } = req.body
       const order = await Order.findById(orderId)
 
       if (!order) throw new NotFoundError()
       if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError()
       if (order.status !== OrderStatus.Created)
         throw new BadRequestError(`Order status is ${order.status}`)
+
+      stripe.charges.create({
+        amount: order.price * 100,
+        source: token,
+        currency: 'usd'
+      })
 
       res.status(201).send({ success: true })
     }
