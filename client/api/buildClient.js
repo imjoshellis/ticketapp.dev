0430d4272dc3725de570815bfb6c43df852e2479
Josh Ellis <josh@imjoshellis.com>
2020-10-26 12:56:32 -0500

    Update base url

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 export const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ticketapp.dev',
       headers: req.headers
     })
   } else {
